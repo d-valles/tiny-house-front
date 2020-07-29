d149995878fd483f9f1d773ac8c65a8254fc4e6b
d-valles <davalles27@gmail.com>
2020-07-29 14:37:08 -0600

    Execute login

--- client/src/sections/Login/index.tsx
+++ client/src/sections/Login/index.tsx
@@ -1,12 +1,81 @@
-import React from "react";
-import { Card, Layout, Typography } from "antd";
+import React, { useEffect, useRef } from "react";
+import { Redirect } from "react-router";
+import { useApolloClient, useMutation } from "@apollo/react-hooks";
+import { Card, Layout, Spin, Typography } from "antd";
+import { AUTH_URL } from "../../lib/graphql/queries/";
+import { LOG_IN } from "../../lib/graphql/mutations/";
+import { AutUrl as AuthUrlData } from "../../lib/graphql/queries/AuthUrl/__generated__/AutUrl";
+import { LogIn as LogInData, LogInVariables } from "../../lib/graphql/mutations/LogIn/__generated__/LogIn";
+import { Viewer } from "../../lib/types"
+import { ErrorBanner } from "../../lib/components";
+import { displaySuccessNotification, displayErrorMessage } from "../../lib/utils";
+
+interface Props {
+    setViewer: (viewer: Viewer) => void;
+}
 
 const { Content } = Layout;
 const { Text, Title } = Typography;
 
-export const Login = () => {
+export const Login = ({ setViewer }: Props) => {
+    const client = useApolloClient();
+    const [logIn, { data: logInData, loading: logInLoading, error: logInError}
+        ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+            onCompleted: (data) => {
+                if (data && data.logIn) {
+                    setViewer(data.logIn);
+                    displaySuccessNotification("You've successfully logged in!");
+                }
+            }
+        });
+    const logInRef = useRef(logIn);
+    useEffect(() => {
+        const code = new URL(window.location.href).searchParams.get("code");
+        if (code) {
+            logInRef.current({
+                variables: {
+                    input: { code }
+                }
+            });
+        }
+    }, []);
+
+    const handleAuthorize = async () => {
+        try {
+            const { data } = await client.query<AuthUrlData>({
+                query: AUTH_URL
+            });
+
+            if (data) {
+                window.location.href = data.authUrl;
+            }
+        } catch {
+            displayErrorMessage(
+                "Sorry! We weren't able to log you in. Please try again later!"
+            );
+        }
+    }
+
+    if (logInLoading) {
+        return (
+            <Content className="log-in">
+                <Spin size="large" tip="Loggin you in..." />
+            </Content>
+        );
+    }
+
+    if (logInData && logInData.logIn) {
+        const { id: viewerId } = logInData.logIn;
+        return <Redirect to={`/user/${viewerId}`}/>
+    }
+
+    const logInErrorBannerElement = logInError ? (
+        <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later!" />
+    ) : null;
+
     return (
         <Content className="log-in">
+            {logInErrorBannerElement}
             <Card className="log-in-card">
                 <div className="log-in-card__intro">
                     <Title level={3} className="log-in-card__intro-title">
@@ -17,7 +86,7 @@ export const Login = () => {
                     </Title>
                     <Text>Sign in with Google to start booking available rentals!</Text>
                 </div>
-                <button className="log-in-card__google-button">
+                <button className="log-in-card__google-button" onClick={handleAuthorize}>
                     <img src="https://d2uusema5elisf.cloudfront.net/courses/tinyhouse-react-masterclass-part-2/module_4/lesson_4.6/public/assets/google_logo.jpg" alt="Google logo" className="logo-in-card__google-button-logo"/>
                     <span className="log-in-card__google-button-text">
                         Sign in with Google
