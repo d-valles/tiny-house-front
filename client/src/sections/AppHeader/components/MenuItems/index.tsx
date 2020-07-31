176b3f5bc4ab4fae4bae7a69386c8bc4c6bf42d1
d-valles <davalles27@gmail.com>
2020-07-31 09:36:34 -0600

    Adds subMenu items

--- client/src/sections/AppHeader/components/MenuItems/index.tsx
+++ client/src/sections/AppHeader/components/MenuItems/index.tsx
@@ -1,11 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button, Menu } from "antd";
-import { HomeOutlined } from "@ant-design/icons";
+import { Avatar, Button, Menu } from "antd";
+import { UserOutlined, HomeOutlined, LogoutOutlined } from "@ant-design/icons";
+import { LOG_OUT } from "../../../../lib/graphql/mutations";
+import { LogOut as LogOutData } from "../../../../lib/graphql/mutations/LogOut/__generated__/LogOut";
+import { Viewer } from "../../../../lib/types";
+import { useMutation } from "react-apollo";
+import { displaySuccessNotification, displayErrorMessage } from "../../../../lib/utils";
 
 const { Item, SubMenu } = Menu;
 
-export const MenuItems = () => {
+interface Props {
+  viewer: Viewer;
+  setViewer: (viewer: Viewer) => void;
+}
+
+export const MenuItems = ({viewer, setViewer}: Props) => {
+  const [logOut] = useMutation<LogOutData>(LOG_OUT, {
+    onCompleted: (data) => {
+      if (data && data.logOut) {
+        setViewer(data.logOut);
+        displaySuccessNotification("You've successfully logged out");
+      }
+    },
+    onError: data => {
+      displayErrorMessage("Sorry! We weren't able to log you out. Please try again later");
+    }
+  });
+
+  const handleLogOut = () => {
+    logOut();
+  }
+
+  const avatarImg = viewer.id && viewer.avatar ? viewer.avatar : "";
+  const subMenuLogin = viewer.id ? (
+    <SubMenu title={<Avatar src={avatarImg}/>}>
+      <Item key="/user">
+        <Link to={`/user/${viewer.id}`} />
+        <UserOutlined />
+        Profile
+      </Item>
+      <Item key="/logout">
+        <div onClick={handleLogOut}>
+          <LogoutOutlined />
+          Logout
+        </div>
+      </Item>
+    </SubMenu>
+  ) : <Item key="/login">
+    <Link to="/login">
+      <Button type="primary">Sign In</Button>
+    </Link>
+  </Item>;
+
   return (
     <Menu mode="horizontal" selectable={false} className="menu">
       <Item key="/host">
@@ -14,11 +61,7 @@ export const MenuItems = () => {
             Host
         </Link>
       </Item>
-      <Item key="/login">
-        <Link to="/login">
-          <Button type="primary">Sign In</Button>
-        </Link>
-      </Item>
+      {subMenuLogin}
     </Menu>
   );
 };
--- client/src/sections/AppHeader/index.tsx
+++ client/src/sections/AppHeader/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Layout } from "antd";
+import { Viewer } from "../../lib/types";
 import { MenuItems } from "./components";
 
 import logo from "./assets/tinyhouse-logo.png";
 
 const { Header } = Layout;
 
-export const AppHeader = () => {
+interface Props {
+    viewer: Viewer;
+    setViewer: (viewer: Viewer) => void;
+}
+
+export const AppHeader = ({viewer, setViewer}: Props) => {
   return (
     <Header className="app-header">
       <div className="app-header__logo-search-section">
@@ -18,7 +24,7 @@ export const AppHeader = () => {
         </div>
       </div>
       <div className="app-header__menu-section">
-        <MenuItems />
+        <MenuItems viewer={viewer} setViewer={setViewer}/>
       </div>
     </Header>
   );
